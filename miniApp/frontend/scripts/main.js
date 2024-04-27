bbe02ca522c554b0156ec1f00241d6524b9bb1d8
Ptah_9 <nilovanton6@gmail.com>
2024-04-27 14:53:21 +0300

    work on connecting lines and usability of blocks

--- miniApp/frontend/scripts/main.js
+++ miniApp/frontend/scripts/main.js
@@ -6,9 +6,108 @@
 let drug = function(selector){
     let block = document.querySelector(selector)
     block.onmousedown = function(event) {
-    
+        let blockSize = {
+            "x": block.getBoundingClientRect().width,
+            "y": block.getBoundingClientRect().height,
+        };
         let shiftX = event.clientX - block.getBoundingClientRect().left;
         let shiftY = event.clientY - block.getBoundingClientRect().top;
+
+        if (shiftX > blockSize["x"]/3 && shiftX < 2*blockSize["x"]/3){
+            if (shiftY < blockSize["y"]/3) {
+                console.log("top");
+
+
+
+
+
+
+
+
+
+
+
+                let from = block,
+                    to = document.querySelector('.to'),
+                    mousePressed = false,
+                    pointFrom,
+                    pointTo,
+                    svg;
+
+                from.addEventListener( 'mousedown', e => {
+                    mousePressed = true;
+                    pointFrom = {
+                        x: e.clientX,
+                        y: e.clientY,
+                    };
+                });
+
+                document.addEventListener( 'mousemove', e => {
+                    if( !mousePressed ) return;
+                    pointTo = {
+                        x: e.clientX,
+                        y: e.clientY,
+                    };
+                    updateLine();
+                });
+
+                document.addEventListener( 'mouseup', e => {
+                    if( !mousePressed ) return;
+                    mousePressed = false;
+                    svg = null;
+                });
+
+                function updateLine(){
+                    if(svg) svg.remove();
+                    
+                    let width = pointTo.x - pointFrom.x,
+                        height = pointTo.y - pointFrom.y;
+                    
+                    svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
+                    svg.style.left = pointFrom.x + 'px';
+                    svg.style.top = pointFrom.y + 'px';
+                    svg.setAttribute( 'width', pointTo.x - pointFrom.x );
+                    svg.setAttribute( 'height', pointTo.y - pointFrom.y );
+                    svg.setAttribute( 'viewBox', `0 0 ${width} ${height}` );
+                    
+                    let path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
+                    path.setAttribute( 'stroke', 'black' );
+                    path.setAttribute( 'd', `M 0,0 L ${width},${height}` );
+                    
+                    svg.appendChild(path);
+                    
+                    document.body.appendChild(svg);
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+            else if(shiftY > 2*blockSize["y"]/3){
+                console.log("bottom")
+            }
+            else {
+                console.log("center")
+            }
+        }
+
+        else{
+
+
         
 
         if (block.classList.contains("non-parent")){
@@ -17,41 +116,53 @@ let drug = function(selector){
         }
         block.style.position = 'absolute';
         block.style.zIndex = 1000;
-        document.body.append(block);
-      
+        document.body.append(block); //TODO: выбрать лучшее место, при помещении в схему перемещать его туда
+
+        function moveAt(pageX, pageY) {
+            block.style.left = pageX - shiftX + 'px';
+            block.style.top = pageY - shiftY + 'px';
+          }
+
         moveAt(event.pageX, event.pageY);
       
-        // переносит мяч на координаты (pageX, pageY),
+        // переносит блок на координаты (pageX, pageY),
         // дополнительно учитывая изначальный сдвиг относительно указателя мыши
-        function moveAt(pageX, pageY) {
-          block.style.left = pageX - shiftX + 'px';
-          block.style.top = pageY - shiftY + 'px';
-        }
+
       
         function onMouseMove(event) {
           moveAt(event.pageX, event.pageY);
         }
       
-        // передвигаем мяч при событии mousemove
+        // передвигаем блок при событии mousemove
         document.addEventListener('mousemove', onMouseMove);
       
-        // отпустить мяч, удалить ненужные обработчики
+        // отпустить блок, удалить ненужные обработчики
         block.onmouseup = function() {
             document.removeEventListener('mousemove', onMouseMove);
             block.onmouseup = null;
-            // questionBlock = document.querySelector('.question-block');
             drug(selector)
-          
-          
         };
         
-      
       };
       
       block.ondragstart = function() {
         return false;
       };
+        }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 drug('.question-block');
 drug('.text-block');
