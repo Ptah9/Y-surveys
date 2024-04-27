c50448ef3d42abd80e88fe3fc79ad88de425772f
Ptah_9 <nilovanton6@gmail.com>
2024-04-28 01:44:08 +0300

    blocks are moved as adequately as possible, the scheme can be scrolled, connection points are added to the blocks

--- miniApp/frontend/scripts/main.js
+++ miniApp/frontend/scripts/main.js
@@ -1,14 +1,15 @@
-// let questionBlock = document.querySelector('.question-block'),
-    // textBlock = document.querySelector('.text-block');
-
-// console.log(questionBlock)
-
-let startBlock = document.querySelector(".scheme")
+let schemeArea = document.querySelector(".scheme");
+let x, y;
+let schemePaddingLeft = Number(window.getComputedStyle(schemeArea, null).getPropertyValue("padding-left").slice(0, -2)),
+    schemePaddingTop = Number(window.getComputedStyle(schemeArea, null).getPropertyValue("padding-top").slice(0, -2));
 
     
 let drug = function(selector){
+
     let blocks = document.querySelectorAll(selector)
+
     let block = blocks[blocks.length - 1];
+    
     block.onmousedown = function(event) {
         let blockSize = {
             "x": block.getBoundingClientRect().width,
@@ -31,57 +32,57 @@ let drug = function(selector){
 
 
 
-                let from = block,
-                    to = document.querySelector('.to'),
-                    mousePressed = false,
-                    pointFrom,
-                    pointTo,
-                    svg;
+                // let from = block,
+                //     to = document.querySelector('.to'),
+                //     mousePressed = false,
+                //     pointFrom,
+                //     pointTo,
+                //     svg;
 
-                from.addEventListener( 'mousedown', e => {
-                    mousePressed = true;
-                    pointFrom = {
-                        x: e.clientX,
-                        y: e.clientY,
-                    };
-                });
+                // from.addEventListener( 'mousedown', e => {
+                //     mousePressed = true;
+                //     pointFrom = {
+                //         x: e.clientX,
+                //         y: e.clientY,
+                //     };
+                // });
 
-                document.addEventListener( 'mousemove', e => {
-                    if( !mousePressed ) return;
-                    pointTo = {
-                        x: e.clientX,
-                        y: e.clientY,
-                    };
-                    updateLine();
-                });
+                // document.addEventListener( 'mousemove', e => {
+                //     if( !mousePressed ) return;
+                //     pointTo = {
+                //         x: e.clientX,
+                //         y: e.clientY,
+                //     };
+                //     updateLine();
+                // });
 
-                document.addEventListener( 'mouseup', e => {
-                    if( !mousePressed ) return;
-                    mousePressed = false;
-                    svg = null;
-                });
+                // document.addEventListener( 'mouseup', e => {
+                //     if( !mousePressed ) return;
+                //     mousePressed = false;
+                //     svg = null;
+                // });
 
-                function updateLine(){
-                    if(svg) svg.remove();
+                // function updateLine(){
+                //     if(svg) svg.remove();
                     
-                    let width = pointTo.x - pointFrom.x,
-                        height = pointTo.y - pointFrom.y;
+                //     let width = pointTo.x - pointFrom.x,
+                //         height = pointTo.y - pointFrom.y;
                     
-                    svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
-                    svg.style.left = pointFrom.x + 'px';
-                    svg.style.top = pointFrom.y + 'px';
-                    svg.setAttribute( 'width', pointTo.x - pointFrom.x );
-                    svg.setAttribute( 'height', pointTo.y - pointFrom.y );
-                    svg.setAttribute( 'viewBox', `0 0 ${width} ${height}` );
+                //     svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
+                //     svg.style.left = pointFrom.x + 'px';
+                //     svg.style.top = pointFrom.y + 'px';
+                //     svg.setAttribute( 'width', pointTo.x - pointFrom.x );
+                //     svg.setAttribute( 'height', pointTo.y - pointFrom.y );
+                //     svg.setAttribute( 'viewBox', `0 0 ${width} ${height}` );
                     
-                    let path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
-                    path.setAttribute( 'stroke', 'black' );
-                    path.setAttribute( 'd', `M 0,0 L ${width},${height}` );
+                //     let path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
+                //     path.setAttribute( 'stroke', 'black' );
+                //     path.setAttribute( 'd', `M 0,0 L ${width},${height}` );
                     
-                    svg.appendChild(path);
+                //     svg.appendChild(path);
                     
-                    document.body.appendChild(svg);
-                }
+                //     document.body.appendChild(svg);
+                // }
 
 
 
@@ -115,53 +116,95 @@ let drug = function(selector){
 
         if (block.classList.contains("non-parent")){
             block.insertAdjacentHTML('beforebegin', block.outerHTML);
-            block.classList.remove("non-parent");
         }
-        block.style.position = 'absolute';
+        block.style.position = 'relative';
         block.style.zIndex = 1000;
-        // document.body.append(block); //TODO: выбрать лучшее место, при помещении в схему перемещать его туда
+        // document.body.append(block); 
 
-        function moveAt(pageX, pageY) {
-            block.style.left = pageX - shiftX + 'px';
-            block.style.top = pageY - shiftY + 'px';
+        function moveAt(movementX, movementY) {
+            if (block.classList.contains("non-parent")){
+                schemeArea.append(block); 
+            }
+            // block.style.left = 0 + "px";
+            // block.style.top = 0 + "px";
+            console.log()
+            let newX = Number(block.style.left.slice(0, -2)) + movementX;
+            let newY = Number(block.style.top.slice(0, -2)) + movementY;
+
+            // if(newX > -schemePaddingLeft) 
+            block.style.left = newX + 'px';
+            // else block.style.left = -schemePaddingLeft
+
+            // if(newY > -schemePaddingTop) 
+            block.style.top = newY + 'px';
+            // else block.style.top = -schemePaddingTop
+
           }
 
-        moveAt(event.pageX, event.pageY);
+        moveAt(event.movementX, event.movementY);
+        if (block.classList.contains("non-parent")){
+            x = block.getBoundingClientRect().x
+            y = block.getBoundingClientRect().y
+            block.style.left = event.pageX - shiftX - x + "px";
+            block.style.top = event.pageY - shiftY - y + "px";
+        }
+        //   let sx = 0
+        //   let sy = 0
+        // function scrollScheme(pageX, pageY){
+        //     if (pageX - shiftX + blockSize["x"] > schemeArea.getBoundingClientRect().width){
+        //         // sy+=1
+        //         sx+=10
+        //         console.log("lol")
+        //         schemeArea.scroll(sx, sy)
+                
+        //         console.log(schemeArea.scrollLeft)
+        //     }
+        // }
+
+
       
         // переносит блок на координаты (pageX, pageY),
         // дополнительно учитывая изначальный сдвиг относительно указателя мыши
 
-      
         function onMouseMove(event) {
-          moveAt(event.pageX, event.pageY);
+          moveAt(event.movementX, event.movementY);
+          block.scrollIntoView({ block: "nearest", inline: "nearest" })
         }
       
         // передвигаем блок при событии mousemove
         document.addEventListener('mousemove', onMouseMove);
       
         // отпустить блок, удалить ненужные обработчики
-        block.onmouseup = function(event) {
+        document.onmouseup = function(event) {
             // block.style.position = 'relative';
 
-            if (block.classList.contains("in-scheme")){
+            // if (block.classList.contains("in-scheme")){
 
 
-            }
-            else{
-                if (event.pageY + blockSize["y"] - shiftY > startBlock.getBoundingClientRect().height){
+            // }
+            // else{
+            //     if (event.pageY + blockSize["y"] - shiftY > schemeArea.getBoundingClientRect().height){
+            //         block.onmouseup = null;
+            //         block.parentNode.removeChild(block);
+            //     }
+            //     else{
                     block.onmouseup = null;
-                    block.parentNode.removeChild(block);
-                }
-                else{
-                    block.onmouseup = null;
-                    startBlock.append(block); 
-                }
-            }
+                    if (block.classList.contains("non-parent")){
+                        schemeArea.append(block); 
+                    }
+                    block.style.left = Number(block.style.left.slice(0, -2)) + event.movementX + 'px';
+                    block.style.top = Number(block.style.top.slice(0, -2)) + event.movementY + 'px';
+                    
 
+            //     }
+            // }
 
             document.removeEventListener('mousemove', onMouseMove);
-            block.onmouseup = null;
-            drug(selector)
+            document.onmouseup = null;
+            if (block.classList.contains("non-parent")){
+                block.classList.remove("non-parent");
+                drug(selector)
+            }
 
         };
         
